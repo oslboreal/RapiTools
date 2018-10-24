ea4819726ba2fef2ab415f2e497ad7c20efb0884
Vallejo Juan Marcos <oslboreal@gmail.com>
2018-10-24 17:02:35 -0300

    MessageQueue adition.

---
+++ Clases/QueueProcessing/EntryMessagesQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapiTools.Clases.QueueProcessing
+{
+    internal class EntryMessagesQueue
+    {
+        // TCP Data.
+        private string ip;
+        private string port;
+
+        // Messaging.
+        private ConcurrentDictionary<int, string> receivedMessages;
+
+        // Processing.
+        private Task subProceso;
+
+        /// <summary>
+        /// Método encargado de escuchar en un puerto determinado aguardando que ingrese un mensaje para encolarlo.
+        /// </summary>
+        public void Listen()
+        {
+        }
+
+        /// <summary>
+        /// Método 
+        /// </summary>
+        /// <returns></returns>
+        private bool Store()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Clases/QueueProcessing/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapiTools.Clases.QueueProcessing
+{
+    internal class Message
+    {
+        private int uid { get; set; }
+        private string body { get; set; } 
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <param name="body"></param>
+        public Message(int uid, string body)
+        {
+            this.uid = uid;
+            this.body = body;
+        }
+
+    }
+}
